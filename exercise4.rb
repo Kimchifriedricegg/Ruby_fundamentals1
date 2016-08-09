b2d64ec58080ce140a81ed60eeccb803d5c0b8e9
Kimchifriedricegg <alexovidenie@gmail.com>
2016-08-09 14:10:15 -0400

    made minor changes to statement

--- exercise4.rb
+++ exercise4.rb
@@ -1,12 +1,13 @@
 for n in 1..100
-  if n % 3 == 0
+  if n % 15 == 0
+  puts "BitMaker"
+
+elsif n % 3 == 0
     puts "Bit"
 
 elsif n % 5 == 0
     puts "Maker"
 
-elsif n % 15 == 0
-    puts "BitMaker"
 
 else puts n
 end
