c5bb56e9d05233ab1564f0f8b9778896c6aa5843
Kimchifriedricegg <alexovidenie@gmail.com>
2016-08-09 13:45:09 -0400

    this is exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+# How would you calculate a good tip for a 55 dollar meal?
+tip = 55 * 0.15
+puts "tip"
+# string and integer conversion
+puts "apple" + 13.to_s
+# string interpolation
+puts "#{45628*7839}"
+# value of exression
+puts (10 < 20 && 30 < 20) || !(10 == 11)
