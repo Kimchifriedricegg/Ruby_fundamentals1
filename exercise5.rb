90f18d1053d74f02ad81127f7af07f8a3231cabf
Kimchifriedricegg <alexovidenie@gmail.com>
2016-08-10 14:21:44 -0400

    made changes to move method first and then puts statements to call

--- exercise5.rb
+++ exercise5.rb
@@ -1,9 +1,11 @@
-puts "what is the temperate in fahrenheit?"
-temp = gets.chomp
+def temperature(temp_in_fahrenheit)
+  puts "the Temperature is #{temp_in_fahrenheit}"
+  temp_in_celsius = temp_in_fahrenheit-32 * 5/9
+  puts temp_in_celsius
 
-def temperature(x)
-puts "the Temperature is #{x}"
-x = "#{temp.to_i}-32 * 5/9"
 end
 
-temperature(x)
+puts "what is the temperature in fahrenheit?"
+temp_in_fahrenheit = gets.chomp.to_i
+
+temperature(temp_in_fahrenheit)
