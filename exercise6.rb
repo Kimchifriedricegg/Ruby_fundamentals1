64a7e75cb6afb966e20cda41150c7d0234ae961f
Kimchifriedricegg <alexovidenie@gmail.com>
2016-08-10 14:55:08 -0400

    added bananas if/else statement

--- exercise6.rb
+++ exercise6.rb
@@ -4,9 +4,22 @@ grocery_list.each do |grocery_list|
   puts "*" + grocery_list
 end
 
-# Add rice to list
-grocery_list << "rice"
+def grocery_list
   puts grocery_list
+end
+
+
+
+# Add rice to list
+  puts grocery_list << "rice"
 
 # Output total number of items on list
   puts grocery_list.count
+
+# check to see if list has bananas, if it does output you need bananas, otherwise output you dont need them
+if grocery_list == "bananas"
+  puts "you need to pick up bananas"
+
+else
+  puts "you do not need bananas today"
+end
