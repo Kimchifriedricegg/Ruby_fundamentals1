3d6a760e04d6fc657b9b98356d65569f829f069c
Kimchifriedricegg <alexovidenie@gmail.com>
2016-08-10 14:40:22 -0400

    added rice to list

---
+++ exercise6.rb
@@ -0,0 +1,9 @@
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+grocery_list.each do |grocery_list|
+  puts "*" + grocery_list
+end
+
+# Add rice to list
+grocery_list << "rice"
+puts grocery_list
